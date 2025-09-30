e0703de019c1495f4a97add89f4c9f5143131356
P.I. Engineering <33100204+pattixkeys@users.noreply.github.com>
2025-09-30 17:48:02 -0400

    Add files via upload

---
+++ Polling Sample/PIHidDotName Csharp Sample/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace PIHidDotName_Csharp_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnEnumerate = new System.Windows.Forms.Button();
+            this.CboDevices = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnUnitID = new System.Windows.Forms.Button();
+            this.TxtSetUnitID = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.BtnCallback = new System.Windows.Forms.Button();
+            this.LblUnitID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BtnPolling = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnEnumerate
+            // 
+            this.BtnEnumerate.Location = new System.Drawing.Point(12, 17);
+            this.BtnEnumerate.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnEnumerate.Name = "BtnEnumerate";
+            this.BtnEnumerate.Size = new System.Drawing.Size(98, 22);
+            this.BtnEnumerate.TabIndex = 0;
+            this.BtnEnumerate.Text = "Enumerate";
+            this.BtnEnumerate.UseVisualStyleBackColor = true;
+            this.BtnEnumerate.Click += new System.EventHandler(this.BtnEnumerate_Click);
+            // 
+            // CboDevices
+            // 
+            this.CboDevices.FormattingEnabled = true;
+            this.CboDevices.Location = new System.Drawing.Point(134, 16);
+            this.CboDevices.Margin = new System.Windows.Forms.Padding(2);
+            this.CboDevices.Name = "CboDevices";
+            this.CboDevices.Size = new System.Drawing.Size(358, 21);
+            this.CboDevices.TabIndex = 1;
+            this.CboDevices.SelectedIndexChanged += new System.EventHandler(this.CboDevices_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 102);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(780, 69);
+            this.listBox1.TabIndex = 6;
+            // 
+            // BtnUnitID
+            // 
+            this.BtnUnitID.Location = new System.Drawing.Point(12, 199);
+            this.BtnUnitID.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnUnitID.Name = "BtnUnitID";
+            this.BtnUnitID.Size = new System.Drawing.Size(92, 22);
+            this.BtnUnitID.TabIndex = 7;
+            this.BtnUnitID.Text = "Write Unit ID*";
+            this.BtnUnitID.UseVisualStyleBackColor = true;
+            this.BtnUnitID.Click += new System.EventHandler(this.BtnUnitID_Click);
+            // 
+            // TxtSetUnitID
+            // 
+            this.TxtSetUnitID.Location = new System.Drawing.Point(115, 198);
+            this.TxtSetUnitID.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtSetUnitID.Name = "TxtSetUnitID";
+            this.TxtSetUnitID.Size = new System.Drawing.Size(30, 20);
+            this.TxtSetUnitID.TabIndex = 8;
+            this.TxtSetUnitID.Text = "1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 262);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(809, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // BtnCallback
+            // 
+            this.BtnCallback.Location = new System.Drawing.Point(12, 64);
+            this.BtnCallback.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnCallback.Name = "BtnCallback";
+            this.BtnCallback.Size = new System.Drawing.Size(119, 22);
+            this.BtnCallback.TabIndex = 10;
+            this.BtnCallback.Text = "Setup for Callback";
+            this.BtnCallback.UseVisualStyleBackColor = true;
+            this.BtnCallback.Click += new System.EventHandler(this.BtnCallback_Click);
+            // 
+            // LblUnitID
+            // 
+            this.LblUnitID.AutoSize = true;
+            this.LblUnitID.Location = new System.Drawing.Point(158, 202);
+            this.LblUnitID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblUnitID.Name = "LblUnitID";
+            this.LblUnitID.Size = new System.Drawing.Size(38, 13);
+            this.LblUnitID.TabIndex = 12;
+            this.LblUnitID.Text = "unit ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "1. Do this first";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "2. Set for data callback and read data";
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(691, 179);
+            this.BtnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(101, 22);
+            this.BtnClear.TabIndex = 23;
+            this.BtnClear.Text = "Clear Listbox";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BtnPolling
+            // 
+            this.BtnPolling.Location = new System.Drawing.Point(182, 64);
+            this.BtnPolling.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnPolling.Name = "BtnPolling";
+            this.BtnPolling.Size = new System.Drawing.Size(79, 22);
+            this.BtnPolling.TabIndex = 25;
+            this.BtnPolling.Text = "Start Polling";
+            this.BtnPolling.UseVisualStyleBackColor = true;
+            this.BtnPolling.Click += new System.EventHandler(this.BtnPolling_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(145, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "OR";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(8, 184);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(80, 13);
+            this.label13.TabIndex = 88;
+            this.label13.Text = "3. Write Unit ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 284);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BtnPolling);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblUnitID);
+            this.Controls.Add(this.BtnCallback);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.TxtSetUnitID);
+            this.Controls.Add(this.BtnUnitID);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.CboDevices);
+            this.Controls.Add(this.BtnEnumerate);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "C# X-keys Polling Sample";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEnumerate;
+        private System.Windows.Forms.ComboBox CboDevices;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnUnitID;
+        private System.Windows.Forms.TextBox TxtSetUnitID;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button BtnCallback;
+        private System.Windows.Forms.Label LblUnitID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BtnPolling;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Polling Sample/PIHidDotName Csharp Sample/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using PIEHidDotNet;
+using PIEHid32Net;
+
+
+namespace PIHidDotName_Csharp_Sample
+{
+    public partial class Form1 : Form, PIEDataHandler, PIEErrorHandler
+    {
+        PIEDevice[] devices;
+
+        int[] cbotodevice = null; //for each item in the CboDevice list maps this index to the device index.  Max devices =100 
+        byte[] wData = null; //write data buffer
+        byte[] lastdata = null;
+        int selecteddevice = -1; //set to the index of CboDevice
+        int singleLED = 0; //indicates whether single LED is running or not
+        //for thread-safe way to call a Windows Forms control
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
+        Control c;
+        //end thread-safe
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnumerate_Click(object sender, EventArgs e)
+        {
+            CboDevices.Items.Clear();
+            cbotodevice = new int[128];  //max # devices = 128
+            //enumerate and setupinterfaces for all devices
+            devices = PIEHid32Net.PIEDevice.EnumeratePIE();
+            if (devices.Length == 0)
+            {
+                toolStripStatusLabel1.Text = "No Devices Found";
+            }
+            else
+            {
+                //System.Media.SystemSounds.Beep.Play(); 
+                int cbocount = 0; //keeps track of how many valid devices were added to the CboDevice box
+                for (int i = 0; i < devices.Length; i++)
+                {
+                    //information about device
+                    //PID = devices[i].Pid);
+                    //HID Usage = devices[i].HidUsage);
+                    //HID Usage Page = devices[i].HidUsagePage);
+                    //HID Version = devices[i].Version); //NOTE: this is NOT the firmware version which is given in the descriptor
+                    int hidusagepg = devices[i].HidUsagePage;
+                    int hidusage = devices[i].HidUsage;
+                    if (devices[i].HidUsagePage == 0xc && devices[i].WriteLength == 36)
+                    {
+                        switch (devices[i].Pid)
+                        {
+                            case 1049:
+                                CboDevices.Items.Add("XK-16 Stick (" + devices[i].Pid + "=PID #1)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1050:
+                                CboDevices.Items.Add("XK-16 Stick (" + devices[i].Pid + "=PID #2)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1051:
+                                CboDevices.Items.Add("XK-16 Stick (" + devices[i].Pid + "=PID #3)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1251:
+                                CboDevices.Items.Add("XK-16 Stick (" + devices[i].Pid + "=PID #4)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1127:
+                                CboDevices.Items.Add("XK-4 Stick (" + devices[i].Pid + "=PID #1)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1128:
+                                CboDevices.Items.Add("XK-4 Stick (" + devices[i].Pid + "=PID #2)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1129:
+                                CboDevices.Items.Add("XK-4 Stick (" + devices[i].Pid + "=PID #3)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1253:
+                                CboDevices.Items.Add("XK-4 Stick (" + devices[i].Pid + "=PID #4)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1130:
+                                CboDevices.Items.Add("XK-8 Stick (" + devices[i].Pid + "=PID #1)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1131:
+                                CboDevices.Items.Add("XK-8 Stick (" + devices[i].Pid + "=PID #2)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1132:
+                                CboDevices.Items.Add("XK-8 Stick (" + devices[i].Pid + "=PID #3)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            case 1252:
+                                CboDevices.Items.Add("XK-8 Stick (" + devices[i].Pid + "=PID #4)");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                            default:
+                                CboDevices.Items.Add(devices[i].ProductString + " (" + devices[i].Pid + ")");
+                                cbotodevice[cbocount] = i;
+                                cbocount++;
+                                break;
+                        }
+                        devices[i].SetupInterface();
+                        devices[i].suppressDuplicateReports = true;
+                    }
+                }
+            }
+            if (CboDevices.Items.Count > 0)
+            {
+                CboDevices.SelectedIndex = 0;
+                selecteddevice = cbotodevice[CboDevices.SelectedIndex];
+                wData = new byte[devices[selecteddevice].WriteLength];//go ahead and setup for write
+                lastdata = new byte[devices[selecteddevice].ReadLength];
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //closeinterfaces on all devices
+            for (int i = 0; i < CboDevices.Items.Count; i++)
+            {
+                devices[cbotodevice[i]].CloseInterface();
+            }
+            System.Environment.Exit(0);
+        }
+
+        private void BtnRed_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnNoLEDS_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnGreen_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void BtnUnitID_Click(object sender, EventArgs e)
+        {
+            //Write Unit ID to currently selected device
+            if (CboDevices.SelectedIndex != -1) //do nothing if not enumerated
+            {
+
+                //write Unit ID given in the TxtSetUnitID box
+                for (int j = 0; j < devices[selecteddevice].WriteLength; j++)
+                {
+                    wData[j] = 0;
+                }
+
+                wData[0] = 0;
+                wData[1] = 189;
+                wData[2] = (byte)(Convert.ToInt16(TxtSetUnitID.Text));
+
+                int result = 404;
+
+                while (result == 404) { result = devices[selecteddevice].WriteData(wData); }
+                if (result != 0)
+                {
+                    toolStripStatusLabel1.Text = "Write Fail: " + result;
+                }
+                else
+                {
+                    toolStripStatusLabel1.Text = "Write Success - Write Unit ID";
+                }
+            }
+        }
+        private void CboDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selecteddevice = cbotodevice[CboDevices.SelectedIndex];
+            wData = new byte[devices[selecteddevice].WriteLength];//size write array 
+        }
+        private void BtnCallback_Click(object sender, EventArgs e)
+        {
+            //setup callback if there are devices found for each device found
+            if (CboDevices.SelectedIndex != -1)
+            {
+                if (CboDevices.SelectedIndex != -1)
+                {
+                    //turn of polling timer
+                    timer1.Enabled = false;
+                    BtnPolling.Text = "Start Polling";
+
+                    for (int i = 0; i < CboDevices.Items.Count; i++)
+                    {
+                        //use the cbotodevice array which contains the mapping of the devices in the CboDevices to the actual device IDs
+                        devices[cbotodevice[i]].SetErrorCallback(this);
+                        devices[cbotodevice[i]].SetDataCallback(this);
+                        devices[cbotodevice[i]].callNever = false;
+                    }
+                }
+            }
+        }
+        //data callback    
+        public void HandlePIEHidData(Byte[] data, PIEDevice sourceDevice, int error)
+        {
+            //check the sourceDevice and make sure it is the same device as selected in CboDevice   
+            if (sourceDevice == devices[selecteddevice])
+            {
+                //read the unit ID
+                c = this.LblUnitID;
+                this.SetText(data[1].ToString());
+
+                //write raw data to listbox1 in HEX
+                String output = "Callback: " + sourceDevice.Pid + ", ID: " + selecteddevice.ToString() + ", data=";
+                for (int i = 0; i < sourceDevice.ReadLength; i++)
+                {
+                    output = output + BinToHex(data[i]) + " ";
+                }
+                this.SetListBox(output);
+            }
+        }
+        //error callback
+        public void HandlePIEHidError(PIEDevice sourceDevice, Int32 error)
+        {
+            this.SetToolStrip("Error: " + error.ToString());
+        }
+        //for threadsafe setting of Windows Forms control
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.c.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.c.Text = text;
+            }
+        }
+        //for threadsafe setting of Windows Forms control
+        private void SetListBox(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.listBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetListBox);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.listBox1.Items.Add(text);
+                this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+            }
+        }
+        //for threadsafe setting of Windows Forms control
+        private void SetToolStrip(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.statusStrip1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetToolStrip);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.toolStripStatusLabel1.Text = text;
+            }
+        }
+
+       
+
+        
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+           
+        }
+
+        private void BtnPolling_Click(object sender, EventArgs e)
+        {
+            if (BtnPolling.Text == "Start Polling")
+            {
+                //turn off callback
+                devices[selecteddevice].callNever = true;
+                
+                timer1.Enabled = true;
+                BtnPolling.Text = "Stop Polling";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                BtnPolling.Text = "Start Polling";
+            }
+
+
+        }
+        public static String BinToHex(Byte value)
+        {
+            StringBuilder sb = new StringBuilder("");
+            sb.Append(value.ToString("X2"));  //the 2 means 2 digits
+            return sb.ToString();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //read data
+            byte[] data = null;
+            while (0 == devices[selecteddevice].ReadData(ref data))
+            {
+                //handle data
+                LblUnitID.Text = data[1].ToString();
+                   
+
+                String output = "Polling: " + devices[selecteddevice].Pid + ", ID: " + selecteddevice.ToString() + ", data=";
+                for (int i = 0; i < devices[selecteddevice].ReadLength; i++)
+                {
+                    output = output + BinToHex(data[i]) + " ";
+                }
+                this.SetListBox(output);
+                            
+            }
+        }
+    }
+    
+}
