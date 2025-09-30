0794691fc6d166f85ed41b5adb4caa65ce3d3a0b
P.I. Engineering <33100204+pattixkeys@users.noreply.github.com>
2025-09-30 17:53:39 -0400

    Add files via upload

---
+++ Device Notification C# Sample/PIHidDotName Csharp Sample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PIHidDotName_Csharp_Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 74);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(824, 173);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 20);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "P.I. Engineering Devices";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(11, 289);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(824, 173);
+            this.listBox2.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Other Devices";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 494);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "C# Device Notification Sample";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Device Notification C# Sample/PIHidDotName Csharp Sample/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;  //in order to use DllImport 
+
+namespace PIHidDotName_Csharp_Sample
+{
+    public partial class Form1 : Form
+    {
+        const int DEVICE_NOTIFY_ALL_INTERFACE_CLASSES = 0x00000004;
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int DBT_DEVTYP_DEVICEINTERFACE = 0x00000005;
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Pack = 1)]
+        public class DeviceBroadcastInterface
+        {
+            public int Size;
+            public int DeviceType;
+            public int Reserved;
+            public Guid ClassGuid;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string Name;
+        }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+
+        public struct DEV_BROADCAST_DEVICEINTERFACE
+        {
+            public uint dbcc_size;
+            public uint dbcc_devicetype;
+            public uint dbcc_reserved;
+            public Guid dbcc_classguid; // GUID
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string dbcc_name1; // tchar[1]
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        protected static extern IntPtr RegisterDeviceNotification(IntPtr hwnd, DeviceBroadcastInterface oInterface, uint nFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        protected static extern bool UnregisterDeviceNotification(IntPtr hHandle);
+        IntPtr deviceNotificationHandle;
+        DeviceBroadcastInterface dbi=new DeviceBroadcastInterface();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //register for device notification
+            int size = Marshal.SizeOf(dbi);
+            dbi.Size = size;
+            dbi.DeviceType = DBT_DEVTYP_DEVICEINTERFACE;
+            deviceNotificationHandle = RegisterDeviceNotification(Handle, dbi, DEVICE_NOTIFY_WINDOW_HANDLE | DEVICE_NOTIFY_ALL_INTERFACE_CLASSES);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //unregister for device notification
+            UnregisterDeviceNotification(Handle);
+           
+            System.Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+      
+        protected override void WndProc(ref Message m)
+        {
+            //you may find these definitions (and more) in dbt.h and winuser.h 
+            const int WM_DEVICECHANGE = 0x0219;
+            // system detected a new device 
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            // system detected a new device 
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+            // device interface class
+            const int DBT_DEVTYP_DEVICEINTERFACE = 0x00000005;
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    int wp =(int) m.WParam;
+                    int lp = (int) m.LParam;
+                    if (wp == DBT_DEVICEARRIVAL || wp == DBT_DEVICEREMOVECOMPLETE)
+                    {
+                        
+                        int devType = Marshal.ReadInt32(m.LParam, 4);
+
+                        switch (devType)
+                        {
+                            case DBT_DEVTYP_DEVICEINTERFACE:
+                                DEV_BROADCAST_DEVICEINTERFACE devint;
+                                devint = (DEV_BROADCAST_DEVICEINTERFACE) Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_DEVICEINTERFACE));
+                                string device = devint.dbcc_name1;
+                                string action = "";
+                                if (wp == DBT_DEVICEARRIVAL) action = "arrival";
+                                if (wp == DBT_DEVICEREMOVECOMPLETE) action = "removal";
+                                if (device.ToUpper().IndexOf("USB#VID_05F3") != -1)  //only want ONE notification per event so ignore all the HID#... ones
+                                {
+                                    //P.I. Engineering device was connected or removed
+                                    this.listBox1.Items.Add(action + "  " + device);
+                                    this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+                                }
+                                else if (device.IndexOf("Vid_05f3") == -1) //non P.I. Engineering devices
+                                {
+                                    this.listBox2.Items.Add(action + "  " + device);
+                                    this.listBox2.SelectedIndex = this.listBox2.Items.Count - 1;
+                                }
+                               
+                                break;
+                        }
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+
+        }
+
+        
+        
+   
+
+        
+       
+       
+    }
+    
+    
+}
